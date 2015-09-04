912099f6a1c41fa1a48a97089d275e6ce0756ab9
Kurt Nelson <kurtisnelson@gmail.com>
2015-09-04 11:57:49 -0500

    Even better changeset!

--- web/views/changeset_view.ex
+++ web/views/changeset_view.ex
@@ -2,15 +2,13 @@ defmodule Bouncer.ChangesetView do
   use Bouncer.Web, :view
 
   def render("error.json", %{changeset: changeset}) do
-    errors = Enum.map(changeset.errors, fn {field, detail} ->
+    %{errors: Enum.map(changeset.errors, fn {field, detail} ->
       %{
         source: %{ pointer: "/data/attributes/#{field}" },
         title: "Invalid Attribute",
         detail: render_detail(detail)
       }
-    end)
-
-    %{errors: errors}
+    end)}
   end
 
   def render_detail({message, values}) do
